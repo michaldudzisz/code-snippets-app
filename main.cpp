f7c17dbc5b9b1dfbe713a482320f98cf9153d97f
Mike <michalkruk199@gmail.com>
2020-11-29 10:08:47 +0100

    simple button

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
+  QPushButton button ("button");
+  button.show();
   return app.exec();
 }
