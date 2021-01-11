0cd9c32ad2dfb3e62c40fe1d380540994522f223
Michal Dudzisz <michaldudzisz@gmail.com>
2021-01-12 00:22:35 +0100

    Dodane komentarze metod publicznych w serwerze

--- server/src/snippet_search_pattern.h
+++ server/src/snippet_search_pattern.h
@@ -51,30 +51,68 @@ public:
 
     /** Checks if all pattern's fields are empty or not */
     bool isEmpty() const;
+
     /** Pareses URL query string into a SnippetSearchPattern object. May throw 
-     * InvalidGetRequestBody exception if the query string is invalid.
-     * */
+     * InvalidGetRequestBody exception if the query string is invalid. 
+     * @param query QueryStringMap from url */
     static SnippetSearchPattern fromQueryString(const QueryStringMap &query);
+
+    /** Parses created_from or created_to string placed in query string 
+     *  into QDateTime object 
+     * @param query String of value of field created_from or created_to 
+     * in the query string from url */
     static QDateTime queryStringToDateTime(const QString &query);
+
+    /** Parses created_from or created_to string placed in query string 
+     *  into QDateTime object 
+     * @param query String of value of field created_from or created_to 
+     * in the query string from url */
     static QDateTime queryStringToDateTime(const QString &&query);
+
+    /** Parses JSON object into a SnippetSearchPattern 
+     * @param obj JSON object to be parsed */
     static SnippetSearchPattern fromJson(QJsonObject &obj);
+
+    /** Parses JSON object into a SnippetSearchPattern 
+     * @param obj JSON object to be parsed */
     static SnippetSearchPattern fromJson(QJsonObject &&obj);
+
+    /** Serializes SnippeSearchPattern object into a JSON object */
     QJsonObject toJson() const;
+
+    /** Serializes date object into a secs-since-unix-epoch format or json NULL */
     static QJsonValue dateToJsonValue(const QDateTime &date);
+    /** Parses QDateTime from its JSON value */
     static QDateTime parseDateTime(const QJsonValue &val);
+    /** Parses QDateTime from its JSON value */
     static QDateTime parseDateTime(const QJsonValue &&val);
+    /** Checks wether JSON object with SnippetSearchPattern is valid 
+     * (contains all necessary fields and fields are appropriate format) */
     static void validateJson(const QJsonObject &obj);
 
+    /** Sets author subsequence, that will be looked for. 
+     * @param author Must fullfil all the conditions for author in Snippet class */
     void setAuthorSubsequence(const QString &author);
+    /** Sets title subsequence, that will be looked for. 
+     * @param title Must fullfil all the conditions for title in Snippet class */
     void setTitleSubsequence(const QString &title);
+    /** Sets creation date, from which server will start to search snippets. */
     void setCreatedFrom(const QDateTime &from);
+    /** Sets creation date, to which server will search snippets. */
     void setCreatedTo(const QDateTime &to);
+    /** Sets programming language, that will be looked for. 
+     * @param lang Must be one of allowed languages */
     void setLang(const QString &lang);
 
+    /** Returns author subseqence that has been set earlier */
     QString authorSubsequence() const;
+    /** Returns title subseqence that has been set earlier */
     QString titleSubsequence() const;
+    /** Returns creation from which to search that has been set earlier */
     QDateTime createdFrom() const;
+    /** Returns creation to which to search that has been set earlier */
     QDateTime createdTo() const;
+    /** Returns programming language that has been set earlier */
     QString lang() const;
 
 private:
